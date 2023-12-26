98baea7ffd3a1a85e7313514c13fe85cc709f97d
Boris LECLERE <Borisleclere@gmail.com>
2023-12-26 14:55:11 +0100

    Missing quick test script

---
+++ quick_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+name=$1
+
+if [ -z "$name" ];
+then
+    name="aioznode"
+fi
+
+# Build specified tag and run for 3s
+docker build . --tag $name && docker run --env RUN_TIMEOUT=3 --rm $name
+
+# Catch error code. 124 is success code
+returnedCode=$?
+
+if [ $returnedCode -eq 124 ]; then
+    echo "Success"
+    exit 0
+fi
+
+exit $returnedCode
