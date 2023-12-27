934cd8cd9cec7c782f2ca473c09acf28cfe07312
Boris LECLERE <Borisleclere@gmail.com>
2023-12-27 11:24:44 +0100

    Set listening endpoint

--- helper.docker.run.sh
+++ helper.docker.run.sh
@@ -4,4 +4,4 @@
 name=$(cat helper.docker.name.txt)
 echo "Launching : docker run $@ --name $name -it $name"
 
-docker run -P $@ --restart=always --name $name -it $name
+docker run -p 1317:1317/tcp -P $@ --restart=always --name $name -it $name
--- tools/start.sh
+++ tools/start.sh
@@ -8,4 +8,4 @@ then
 fi
 
 echo "Starting node for ${RUN_TIMEOUT}s..."
-timeout "${RUN_TIMEOUT}s" $BINARY_NAME start --home $NODEDATA_FOLDER --priv-key-file $PRIV_KEY_FILE
+timeout "${RUN_TIMEOUT}s" $BINARY_NAME start --home $NODEDATA_FOLDER --priv-key-file $PRIV_KEY_FILE --laddr tcp://0.0.0.0:1317
