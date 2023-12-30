92dc8b2a667b5bb06bb5f32e065e2c3c6271100d
Boris LECLERE <Borisleclere@gmail.com>
2023-12-30 16:36:32 +0100

    Fix error code

--- quick_test.sh
+++ quick_test.sh
@@ -12,9 +12,10 @@ docker build . --tag $name && docker run --env RUN_TIMEOUT=3 --rm $name
 # Catch error code. 124 is success code
 returnedCode=$?
 
-if [ $returnedCode -eq 124 ]; then
+if [ $returnedCode -eq 143 ]; then
     echo "Success"
     exit 0
 fi
 
+echo Returned code : $returnedCode
 exit $returnedCode
